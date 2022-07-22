1fc17b83245e47bc67e7ff40dcdbed0b86f70283
guliter <86565392+guliter@users.noreply.github.com>
2022-07-22 11:06:18 +0800

    Update aws.sh

--- AWS/aws.sh
+++ AWS/aws.sh
@@ -57,7 +57,7 @@ pack() {
 echo
 redbg "【AWS-Panel】-默认面板:http://${ip}:8011 【admin admin123456】"
 echo
-green"命令提示:
+green "命令提示:
 echo
 1.|sudo yum install epel-release;yum install screen&&screen -S aws
 2.|cd /root/aws-Panel&&./AWS-Panel-linux-amd64 
